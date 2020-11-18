7f06e6ee529a72195cd5c650701d76ffdb07f57d
发声的沉默者 <hezhiqianye@gmail.com>
2020-11-18 17:09:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def login():
 
 def get_blog():
     global blog
-    blog = user.get_repo('GITHUB_USER/LiuJiang-Blog')
+    blog = user.get_repo('xuya227939/LiuJiang-Blog')
 
 
 def bundle_summary_section():
