312974acbd71a8a34ff62b4a4f675008dcb9585f
xuya227939 <hezhiqianye@gmail.com>
2024-04-21 20:10:36 +0800

    chore: update themee

--- src/theme.config.ts
+++ src/theme.config.ts
@@ -1,10 +1,10 @@
 export const THEME_CONFIG: App.Locals["config"] = {
   /** blog title */
-  title: "江辰的博客",
+  title: "的博客",
   /** your name */
   author: "江辰",
   /** website description */
-  desc: "无所吊谓",
+  desc: "博客|江辰|独立开发者|前网易高级开发工程师",
   /** your deployed domain */
   website: "https://github.com/xuya227939",
   /** your locale */
